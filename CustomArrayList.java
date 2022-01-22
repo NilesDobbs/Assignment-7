6798f0cca5f9f623dd04c3fa4c824c9ee2b1deb4
NilesDobbs <83557367+NilesDobbs@users.noreply.github.com>
2022-01-22 10:27:19 -0500

    Create CustomArrayList.java

---
+++ CustomArrayList.java
@@ -0,0 +1,60 @@
+package CustomTest;
+
+import java.util.Arrays;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Integer LengthofArray = 10;
+	Object[] items = new Object[LengthofArray];
+	Integer itemCounter = 0;
+	
+	@Override
+	public boolean add(T item) {
+		if (getSize() == (LengthofArray)) {
+			items = Arrays.copyOf(items, getSize() * 2);
+			LengthofArray = getSize() * 2;
+		}
+		items[itemCounter++] = item;
+		return true;
+	}
+	
+	@Override
+	public int getSize() {
+		
+		return itemCounter;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException {
+		//Return the element of the array.
+		return (T) items[index];
+	}
+	
+	@Override
+	public boolean add(int index, T item) throws ArrayIndexOutOfBoundsException, IndexOutOfBoundsException {
+		if (getSize() == (LengthofArray)) {
+			items = Arrays.copyOf(items, getSize() * 2);
+			LengthofArray = getSize() * 2;
+		}
+		
+			for(int i = itemCounter; i > index; i--) {
+				items[i] = items[i-1];
+			}
+			items[index] = item;
+			itemCounter++;
+			return true;
+		}
+	
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+		if(index > LengthofArray && index >= 0) {
+			throw new IndexOutOfBoundsException(index);
+		}
+		
+		for(int i = index; i < items.length-1; i++) {
+			items[i] = items[i + 1];
+		}
+			itemCounter--;
+		return null;
+	}
+}
